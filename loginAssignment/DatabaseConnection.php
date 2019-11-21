34b836919284e9de1a8ea012d1159a6faf88c4ab
Shane Beyer <54415734+Beyer98@users.noreply.github.com>
2019-11-21 17:27:52 -0600

    Add files via upload

---
+++ loginAssignment/DatabaseConnection.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+	$servername = "localhost";
+
+	$username = "slbeyer2_wdv341";
+
+	$password = "Pandapo1?";
+
+
+
+	try {
+
+   		$conn = new PDO("mysql:host=$servername;dbname=slbeyer2_wdv341", $username, $password);
+
+   		// set the PDO error mode to exception
+
+   		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   		echo "<script>console.log('Connected successfully') </script>";
+
+  	 }
+
+	catch(PDOException $e)
+
+   	{
+
+   		echo "Connection failed: " . $e->getMessage();
+
+   	}
+
+?>
