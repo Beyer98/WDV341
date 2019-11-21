76888f443ecac37a4ffde2304bf48aa27f5517d4
Shane Beyer <54415734+Beyer98@users.noreply.github.com>
2019-11-21 17:34:10 -0600

    Update formHandler.php

--- formHandler.php
+++ formHandler.php
@@ -1,56 +1,99 @@
 <?php
 
-	$tableBody = "";		
-	
-	foreach($_POST as $key => $value)		
-	{
-		$tableBody .= "<tr>";				
-		$tableBody .= "<td>$key</td>";		
-		$tableBody .= "<td>$value</td>";	
-		$tableBody .= "</tr>";				
-	} 
-	
+//Model-Controller Area.  The PHP processing code goes in this area.
 
-	
-	$inFirstName = $_POST["firstName"];		
-	$inLastName = $_POST["lastName"];		
-	$inSchool = $_POST["school"];			
-	$inPHP = $_POST["php"];
-	$inWhichClass = $_POST["whichClass"];
+//Method 1.  This uses a loop to read each set of name-value pairs stored in the $_POST array
+
+$tableBody = "";		//use a variable to store the body of the table being built by the script
+
+foreach($_POST as $key => $value)		//This will loop through each name-value in the $_POST array
+
+	{
+
+		$tableBody .= "<tr>";				//formats beginning of the row
+
+		$tableBody .= "<td>$key</td>";		//dsiplay the name of the name-value pair from the form
+
+		$tableBody .= "<td>$value</td>";	//dispaly the value of the name-value pair from the form
+
+		$tableBody .= "</tr>";				//End this row
+
+	}
+
+//Method 2.  This method pulls the individual name-value pairs from the $_POST using the name
+
+//as the key in an associative array.
+
+	$inFirstName = $_POST["firstName"];		//Get the value entered in the first name field
+
+	$inLastName = $_POST["lastName"];		//Get the value entered in the last name field
+
+	$inSchool = $_POST["school"];			//Get the value entered in the school field
+
+	$inClass = $_POST["class"];
+
+	$inCheckbox1 = $_POST["checkbox1"];
+
+	$inCheckbox2 = $_POST["checkbox2"];
+
+	$inSelect = $_POST["selected"];
 
 ?>
+
 <!DOCTYPE html>
+
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <title>WDV 341 Intro PHP - Code Example</title>
-	<style>
-		
-	</style>
 
 </head>
 
 <body>
+
 <h1>WDV341 Intro PHP</h1>
-<h2>Form Handler</h2>
+
+<h2>Form Handler Result Page - Code Example</h2>
+
 <p>This page displays the results of the Server side processing. </p>
-<h3>Display the values:</h3>
+
+<p>The PHP page has been formatted to use the Model-View-Controller (MVC) concepts. </p>
+
+<h3>Display the values from the form using Method 1. Uses a loop to process through the $_POST array</h3>
+
 <p>
+
 	<table border='a'>
+
     <tr>
+
     	<th>Field Name</th>
+
         <th>Value of Field</th>
+
     </tr>
+
 	<?php echo $tableBody;  ?>
+
 	</table>
+
 </p>
-<h3>Display the values from the form.</h3>
+
+<h3>Display the values from the form using Method 2. Displays the individual values.</h3>
+
 <p>School: <?php echo $inSchool; ?></p>
+
 <p>First Name: <?php echo $inFirstName; ?></p>
+
 <p>Last Name: <?php echo $inLastName; ?></p>
-<p>Relationship with PHP: <?php echo $inPHP; ?></p>
-<p>Which Class: <?php echo $inWhichClass; ?></p>
 
+<p>Class: <?php echo $inClass; ?></p>
 
+<p>CheckBox: <?php echo $inCheckbox1." ". $inCheckbox2;?></p>
+
+<p>DropDown List: <?php echo $inSelect; ?></p>
 
 </body>
+
 </html>
