f8d408ce6e3db87c1da097e54ea5ca60dee2d3d0
Shane Beyer <54415734+Beyer98@users.noreply.github.com>
2019-12-10 09:58:36 -0600

    Update and rename DatabaseConnection.php to connectPDO.php

---
+++ loginAssignment/connectPDO.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+$serverName = "localhost";
+
+$databaseUsername = "root";
+
+$databasePassword = "";
+
+$database = "wdv341";
+
+
+
+$ConnectionError = "";
+
+
+
+try {
+
+    $conn = new PDO("mysql:host=$serverName;dbname=$database", $databaseUsername, $databasePassword);
+
+    // set the PDO error mode to exception
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //echo "Connected successfully"; 
+
+}
+
+catch(PDOException $e)
+
+{
+
+    $ConnectionError = $e;
+
+}
+
+?>
